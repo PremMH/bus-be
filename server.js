c8f2ba55b20c753ff92584c9a3b28d05dea14e83
prem.hagaragi <prem.hagaragi@minsyst.com>
2024-04-14 12:01:20 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+    res.send('Hello from the server!!');
+    
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
+
